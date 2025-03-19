c1e4261a00672f266e8b83b957e28ac11e334d3b
Chaeyeong_10 <147240485+Chaeyeonggg@users.noreply.github.com>
2025-03-20 00:16:52 +0900

    [Bronze V] Title: 문자와 문자열, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,19 @@
+//import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		String S = scanner.next();
+		int i = scanner.nextInt();
+		
+		char c = S.charAt(i-1);
+		
+		System.out.println(c);
+		
+		
+		scanner.close();
+	}
+}
