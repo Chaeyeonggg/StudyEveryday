7c636ebc898e195451e6c9efb440592eaa79da37
Chaeyeong_10 <147240485+Chaeyeonggg@users.noreply.github.com>
2025-03-20 00:14:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	String c = scanner.next();
+    	char ch = c.charAt(0);
+    	int a = ch;
+    	
+    	System.out.println(a);
+    	
+    	scanner.close();
+    }
+}
