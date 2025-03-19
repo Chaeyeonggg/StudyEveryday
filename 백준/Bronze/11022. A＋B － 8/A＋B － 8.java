e53ad4ecc2091ce00e304c73e1a17af209a61cb3
Chaeyeong_10 <147240485+Chaeyeonggg@users.noreply.github.com>
2025-03-20 00:14:41 +0900

    [Bronze V] Title: A+B - 8, Time: 220 ms, Memory: 18940 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+		int i = 1;
+		while(T!=0) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			int sum = A+B;
+			System.out.println("Case #"+i+": "+A+" + "+B+" = " + sum);
+			i++;
+			T--;
+		}
+		scanner.close();
+	}
+}
+
