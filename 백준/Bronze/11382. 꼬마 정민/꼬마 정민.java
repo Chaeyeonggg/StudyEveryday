0d1a69af4039855d3291758783326506fe1386cf
Chaeyeong_10 <147240485+Chaeyeonggg@users.noreply.github.com>
2025-03-20 00:23:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner scanner = new Scanner(System.in);
+		
+			long x = scanner.nextLong();
+			long y = scanner.nextLong();
+			long z = scanner.nextLong();
+			
+			long sum = x+y+z;
+			System.out.println(sum);
+			scanner.close();
+
+
+
+	}
+}
